1b6563cb4e4044c37ee390c6ad08632adec31dd7
Ahmad <40579808+AHMAD-879@users.noreply.github.com>
2021-12-13 22:19:21 +0300

    Changed if-else to switch, changed replace to replaceFirst

--- Main.java
+++ Main.java
@@ -13,77 +13,78 @@ public class Main {
         Trie<String> trie = new Trie<>(); // a trie for the user
         String inp; // for containing user input
         Scanner input = new Scanner(System.in);
-        String choice = "";
-        String menu = "\nTRIE PROJECT: Choose one of the following..\n" +
-                "1) Create an empty trie\n" +
-                "2) Create a trie with initial letters\n" +
-                "3) Insert a word\n" +
-                "4) Delete a word\n" +
-                "5) List all words that begin with a prefix\n" +
-                "6) Size of the trie\n" +
-                "7) Search for a word\n" +
-                "8) Print all words\n" +
-                "9) End\n" +
-                "Enter your choice: ";
+        String choice;
+        String menu = """
+
+                TRIE PROJECT: Choose one of the following..
+                1) Create an empty trie
+                2) Create a trie with initial letters
+                3) Insert a word
+                4) Delete a word
+                5) List all words that begin with a prefix
+                6) Size of the trie
+                7) Search for a word
+                8) Print all words
+                9) End
+                Enter your choice:\s""";
         // loop while the user does not choose to exit
         boolean ended = false;
         while (!ended) {  // while the user does not choose to exit
             System.out.print(menu);
             choice = input.next();
-            if (choice.equals("1")) {  // Create an empty Trie
-                trie = new Trie<>();
-                System.out.println("New empty trie created.");
-            }
-            else if (choice.equals("2")) {  // Create a trie with initial letters
-                trie = new Trie<>();
-                System.out.print("Enter your list of letters> ");
-                input.nextLine(); // to get rid of the remaining portion of the previous input line
-                inp = input.nextLine().toUpperCase();
-                String[] permutations = diffLengthPermutations(inp); // all possible permutations with different lengths
-                for (String word : permutations) // for each word from the permutations;
-                    if (dictionary.contains(word)) // if the word is in the dictionary;
-                        trie.insert(word);  // add the word to the trie
-                System.out.printf("Trie with initials %s created.\n", inp);
-            }
-            else if (choice.equals("3")) { // Insert a word
-                System.out.print("Enter a word to insert> ");
-                inp = input.next().toUpperCase().replaceAll("[^A-Z]", ""); // change to uppercase, then remove all non-alphabetic characters;
-                trie.insert(inp);
-                System.out.printf("Word %s added.\n", inp);
-            }
-            else if (choice.equals("4")) { // Delete a word
-                System.out.print("Enter a word to delete> ");
-                inp = input.next().toUpperCase();
-                trie.delete(inp);
-                System.out.printf("Word %s deleted.\n", inp);
-            }
-            else if (choice.equals("5")) { // List all words that begin with a prefix
-                System.out.print("Enter a prefix> ");
-                inp = input.next().toUpperCase();
-                System.out.printf("All words that start with %s:\n", inp);
-                System.out.println(Arrays.toString(trie.allWordsPrefix(inp)));
-            }
-            else if (choice.equals("6")) { // Size of the trie
-                System.out.println("Size of the trie: " + trie.size());
-            }
-            else if (choice.equals("7")) { // Search for a word
-                System.out.print("Enter a word to search for> ");
-                inp = input.next().toUpperCase();
-                if (trie.contains(inp))
-                    System.out.printf("Word Found: %s\n", inp);
-                else
-                    System.out.printf("Word %s not found.\n", inp);
-            }
-            else if (choice.equals("8")) { // Print all words in the trie
-                String[] allWords = trie.allWords();
-                System.out.println("All words in the trie:\n" + Arrays.toString(allWords));
-            }
-            else if (choice.equals("9")) { // End
-                System.out.println("Exiting program..");
-                ended = true;
-            }
-            else {
-            System.out.println("Invalid input. Please try again.");
+            switch (choice) {
+                case "1" -> {   // Create an empty Trie
+                    trie = new Trie<>();
+                    System.out.println("New empty trie created.");
+                }
+                case "2" -> {   // Create a trie with initial letters
+                    trie = new Trie<>();
+                    System.out.print("Enter your list of letters> ");
+                    input.nextLine(); // to get rid of the remaining portion of the previous input line
+                    inp = input.nextLine().toUpperCase().replaceAll(" ", ""); // convert to uppercase, and remove whitespaces
+                    String[] permutations = diffLengthPermutations(inp); // all possible permutations with different lengths
+                    for (String word : permutations) // for each word from the permutations;
+                        if (dictionary.contains(word)) // if the word is in the dictionary;
+                            trie.insert(word);  // add the word to the trie
+
+                    System.out.printf("Trie with initials %s created.\n", inp);
+                }
+                case "3" -> {  // Insert a word
+                    System.out.print("Enter a word to insert> ");
+                    inp = input.next().toUpperCase().replaceAll("[^A-Z]", ""); // change to uppercase, then remove all non-alphabetic characters;
+                    trie.insert(inp);
+                    System.out.printf("Word %s added.\n", inp);
+                }
+                case "4" -> {  // Delete a word
+                    System.out.print("Enter a word to delete> ");
+                    inp = input.next().toUpperCase();
+                    trie.delete(inp);
+                }
+                case "5" -> {  // List all words that begin with a prefix
+                    System.out.print("Enter a prefix> ");
+                    inp = input.next().toUpperCase();
+                    System.out.printf("All words that start with %s:\n", inp);
+                    System.out.println(Arrays.toString(trie.allWordsPrefix(inp)));
+                }
+                case "6" ->  // Size of the trie
+                        System.out.println("Size of the trie: " + trie.size());
+                case "7" -> {  // Search for a word
+                    System.out.print("Enter a word to search for> ");
+                    inp = input.next().toUpperCase();
+                    if (trie.contains(inp))
+                        System.out.printf("Word Found: %s\n", inp);
+                    else
+                        System.out.printf("Word %s not found.\n", inp);
+                }
+                case "8" -> {  // Print all words in the trie
+                    String[] allWords = trie.allWords();
+                    System.out.println("All words in the trie:\n" + Arrays.toString(allWords));
+                }
+                case "9" -> {  // End
+                    System.out.println("Exiting program..");
+                    ended = true;
+                }
+                default -> System.out.println("Invalid input. Please try again.");
             }
         }
         input.close();  // close the scanner
@@ -111,11 +112,12 @@ public class Main {
         }
         else { // if the word has more than 2 characters
             for (String letter : s.split("")) { // for each letter in the word
-                String[] permsForOtherLetters = permutations(s.replace(letter, "")); // get the permutations with the other letters
+                String[] permsForOtherLetters = permutations(s.replaceFirst(letter, "")); // get the permutations with the other letters
                 for (String perm : permsForOtherLetters) // for each of the permutations;
                     words.add(letter + perm); // add the current letter to the permutation, then add it to the list
             }
         }
+
         return words.toArray(new String[0]); // return the list as an array
     }
 }
